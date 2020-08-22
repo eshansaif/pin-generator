767cdb059f7fd470cb2e0b0b9df8e6e9f49175da
eshansaif <35582062+eshansaif@users.noreply.github.com>
2020-08-23 01:15:38 +0600

    script are separated and some optimization is done

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-// console.log(Math.floor(1000 + Math.random() * 9000));
+//Generate pin number
+function generateRandomPin() {
+    const randomNumber = Math.floor(1000 + Math.random() * 9000);
+    document.getElementById("display-random-number").value = randomNumber;
+}
+
+//Display number in the input field
+function displayNumber(num) {
+    document.getElementById("display-input-number").value = document.getElementById("display-input-number").value +  num;
+}
+
+//Submit pin number to match the pin is valid or not
+function submitPinNumber() {
+    const displayRandomNumber = document.getElementById("display-random-number").value;
+    const displayInputNumber = document.getElementById("display-input-number").value;
+
+    if (displayInputNumber == "" || displayInputNumber.length != 4) {
+        alert("Please Enter the 4 Digit Pin Number!");
+        return;
+    }
+
+    if(displayRandomNumber == displayInputNumber){
+        document.getElementById("green-sign").style.display = "block";
+        document.getElementById("red-signal").style.display = "none";
+    }else{
+        document.getElementById("green-sign").style.display = "none";
+        document.getElementById("red-signal").style.display = "block";
+    }
+}
+
+//clear input field
+document.getElementById("clear").addEventListener("click",function(){
+    document.getElementById("display-input-number").value = "";
+})
+
+
+//backspace input field
+function backspace() {
+    var value = document.getElementById("display-input-number").value;
+    document.getElementById("display-input-number").value = value.substr(0, value.length - 1);
+}
